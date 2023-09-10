f389d764ccc31e21bbf7ac1ec0739dd087f56200
Hossein Pira <120723120+ReactMVC@users.noreply.github.com>
2023-09-10 10:11:05 -0400

    Update EasySQL.php

--- EasySQL.php
+++ EasySQL.php
@@ -374,16 +374,4 @@ class EasySQL
             return $stmt->execute($values);
         }
     }
-
-    /**
-     * Sets the character set for the database connection.
-     *
-     * @param string $charset The character set to use.
-     */
-    public function setCharset($charset)
-    {
-        $this->charset = $charset;
-        $dsn = "mysql:host=$this->host;dbname=$this->database;charset=$this->charset";
-        $this->pdo = new PDO($dsn, $this->username, $this->password);
-    }
 }
